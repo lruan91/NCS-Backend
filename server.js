ef54c52e71eb91727d3abd895fd6d08956937c6f
Lynnea <lruan.dev@gmail.com>
2022-06-17 20:50:13 -0400

    Express Simple REST

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const morgan = require('morgan');
 
@@ -6,6 +7,55 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+// send back plain text in the body
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+// the post will carry some information in the body of the message
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+// going to reject
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not support on /campsites');
+});
+
+// normally a dangerous application. Usually only priviledged users
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+// allows to store whatever the client sends a part of the path after the / as a route parameter named campsiteId
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+// deleting a specific campsite and not all of them
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 app.use(express.static(__dirname + '/public'));
 
